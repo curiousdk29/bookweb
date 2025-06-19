19624269b31484b7755c1bc8351a1bf6280566ca
curiousdk29 <145142203+curiousdk29@users.noreply.github.com>
2025-06-20 00:13:20 +0530

    Update settings.py

--- bookapp/settings.py
+++ bookapp/settings.py
@@ -134,8 +134,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    'RESOURCE_TYPE': 'raw'
 }
 
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+
